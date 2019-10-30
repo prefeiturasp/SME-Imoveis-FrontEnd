d7491ad427a6003291ffb74a3afb98cd213c82c2
Cesar Augusto <cesar.bruschetta@amcom.com.br>
2019-10-30 08:43:10 -0300

    adição de teste unitarios

--- src/components/Input/AutoComplete/index.test.js
+++ src/components/Input/AutoComplete/index.test.js
@@ -1,14 +1,39 @@
 import { mount } from "enzyme";
 import React from "react";
+import { required } from "helpers/fieldValidators";
 import { AutoComplete } from "./index";
 
 describe("test <AutoComplete>", () => {
   let wrapper;
+  let onChange = jest.fn();
+  let input = { onChange, value: "" };
+
   beforeAll(() => {
-    wrapper = mount(<AutoComplete value="" input={{ value: "" }} />);
+    const props = {
+      meta: {
+        touched: true,
+        error: "This field is required"
+      }
+    };
+    wrapper = mount(
+      <AutoComplete
+        className="teste"
+        name="myCombo"
+        input={input}
+        label="mylabel"
+        required
+        value=""
+        validate={required}
+        {...props}
+      />
+    );
   });
 
   it("renders component", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("Change Value", () => {
+    wrapper.find("input").simulate("change", { target: { value: "Rua A" } });
   });
 });
--- src/components/Input/FileUpload.test.js
+++ src/components/Input/FileUpload.test.js
@@ -5,9 +5,14 @@ import { required } from "helpers/fieldValidators";
 import { FileUpload } from "./FileUpload";
 
 describe("test <FileUpload>", () => {
+  window.URL.createObjectURL = jest.fn();
   let wrapper;
   let onChange = jest.fn();
   let input = { onChange, value: "" };
+
+  afterEach(() => {
+    window.URL.createObjectURL.mockReset();
+  });
 
   beforeEach(() => {
     const props = {
@@ -29,8 +34,20 @@ describe("test <FileUpload>", () => {
       />
     );
   });
+  const file = new File(["dummy content"], "example.png", {
+    type: "image/png"
+  });
+  const event = {
+    target: {
+      files: [file]
+    }
+  };
 
   it("renders component", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("Change Value", () => {
+    wrapper.find("input").simulate("change", event);
   });
 });
