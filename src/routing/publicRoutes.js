9e5ae0d2951dad5f04b22db6b75149d38813bb51
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-10 11:26:37 -0300

    fix rotas

--- src/routing/publicRoutes.js
+++ src/routing/publicRoutes.js
@@ -1,9 +1,27 @@
 import Home from "pages/Home";
+import CadastroImovel from "pages/CadastroImovel";
+import { Login } from "pages/Login";
+import { SemPermissao } from "pages/SemPermissao";
 
 export const publicRoutes = [
   {
     path: "/",
     component: Home,
+    exact: true,
+  },
+  {
+    path: "/login",
+    component: Login,
+    exact: true,
+  },
+  {
+    path: "/sem-permissao",
+    component: SemPermissao,
+    exact: true,
+  },
+  {
+    path: "/cadastro-imovel",
+    component: CadastroImovel,
     exact: true,
   },
 ];
