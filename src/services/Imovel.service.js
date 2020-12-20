6b752e489ae9f885df0bb958743d41daeed932f9
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-20 20:46:22 -0300

    fluxo de status

--- src/services/Imovel.service.js
+++ src/services/Imovel.service.js
@@ -1,5 +1,9 @@
 import api from "./_base";
 
-export const cadastrarImovel = async payload => {
+export const cadastrarImovel = async (payload) => {
   return await api.post("cadastro-imovel/", payload);
+};
+
+export const getImovel = async (uuid) => {
+  return await api.get(`cadastro-imovel/${uuid}/`);
 };
