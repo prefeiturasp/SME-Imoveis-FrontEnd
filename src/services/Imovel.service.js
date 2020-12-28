2e196f855c1cbb9b6a025cac1442a6292a74c736
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-28 17:35:32 -0300

    ajuste token update imovel

--- src/services/Imovel.service.js
+++ src/services/Imovel.service.js
@@ -1,3 +1,5 @@
+import endPontsConstants from "constants/endPonts.constants";
+import { getToken } from "./auth.service";
 import api from "./_base";
 
 export const cadastrarImovel = async (payload) => {
@@ -5,8 +7,29 @@ export const cadastrarImovel = async (payload) => {
 };
 
 export const updateImovel = async (id, payload) => {
-  return await api.patch(`cadastro-imovel/${id}/`, payload);
+  const url = `${endPontsConstants.API_URL}/cadastro-imovel/${id}/`;
+  let status = 0;
+  return fetch(url, {
+    headers: {
+      Authorization: `JWT ${getToken()}`,
+      "Accept-Language": "pt-br",
+      "Content-Type": "application/json",
+    },
+    method: "PATCH",
+    body: JSON.stringify(payload),
+  })
+    .then((res) => {
+      status = res.status;
+      return res.json();
+    })
+    .then((data) => {
+      return { data: data, status: status };
+    })
+    .catch((error) => {
+      return error;
+    });
 };
+
 
 export const getImovel = async (uuid) => {
   return await api.get(`cadastro-imovel/${uuid}/`);
