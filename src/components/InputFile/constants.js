2f48bf2d18fba73829ca5bf43c10d6316cc25603
Calvin Rossinhole <calvin.masters@gmail.com>
2020-12-28 15:03:11 -0300

    anexos finalizado

---
+++ src/components/InputFile/constants.js
@@ -0,0 +1,2 @@
+export const DOIS_MB = 2097152;
+export const DEZ_MB = 10485760;
--- src/pages/AreaLogada/DetalhamentoCadastro/componentes/Anexos/index.jsx
+++ src/pages/AreaLogada/DetalhamentoCadastro/componentes/Anexos/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import HTTP_STATUS from "http-status-codes";
 import Botao from "components/Botao";
 import {
@@ -6,9 +6,12 @@ import {
   BUTTON_STYLE,
   BUTTON_TYPE,
 } from "components/Botao/constants";
-import { deleteAnexo } from "services/anexos.service";
+import { setAnexo, deleteAnexo } from "services/anexos.service";
+import { Field } from "react-final-form";
 import { toastError, toastSuccess } from "components/Toast/dialogs";
 import { getImovel } from "services/Imovel.service";
+import InputFile from "components/InputFile";
+import { TIPO_DOCUMENTO } from "./constans";
 
 export const Anexos = ({
   cadastro,
@@ -39,6 +42,31 @@ export const Anexos = ({
     }
   };
 
+  const uploadAnexo = async (e, tipo) => {
+    const arquivoAnexo = {
+      ...e[0],
+      tipo_documento: tipo,
+      imovel: cadastro.id,
+    };
+    setAnexo(arquivoAnexo).then((response) => {
+      if (response.status === HTTP_STATUS.CREATED) {
+        getImovel(cadastro.id)
+          .then((response) => {
+            if (response.status === HTTP_STATUS.OK) {
+              setPropsCadastro(response.data);
+            } else {
+              setPropsErro(true);
+            }
+          })
+          .catch(() => {
+            setPropsErro(true);
+          });
+      } else {
+        toastError("Erro ao dar upload no arquivo");
+      }
+    });
+  };
+
   return (
     <>
       <div className="title mb-3">Anexos e relatórios</div>
@@ -48,17 +76,49 @@ export const Anexos = ({
           (anexo) => anexo.get_tipo_documento_display === "Fotos da Fachada"
         )
         .map((anexo, key) => {
-          return (
+          return !editar ? (
             <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
               <Botao
                 style={BUTTON_STYLE.BLUE_OUTLINE}
                 type={BUTTON_TYPE.BUTTON}
-                className="mr-3"
                 texto={`Anexo ${key + 1}`}
+                className="mr-3"
               />
             </a>
+          ) : (
+            <>
+              <Botao
+                style={BUTTON_STYLE.BLUE_OUTLINE}
+                type={BUTTON_TYPE.BUTTON}
+                icon={BUTTON_ICON.TRASH}
+                className="br-none"
+                onClick={() => removerAnexo(anexo.uuid)}
+              />
+              <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
+                <Botao
+                  style={BUTTON_STYLE.BLUE_OUTLINE}
+                  type={BUTTON_TYPE.BUTTON}
+                  icon={BUTTON_ICON.DOWNLOAD}
+                  className="mr-3"
+                />
+              </a>
+            </>
           );
         })}
+      {editar && (
+        <Field
+          component={InputFile}
+          className="inputfile"
+          texto="Adicionar"
+          name="files"
+          accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
+          setFiles={(files) =>
+            uploadAnexo(files, TIPO_DOCUMENTO.FOTOS_DA_FACHADA)
+          }
+          toastSuccess={"Anexo do documento incluído com sucesso!"}
+          multiple
+        />
+      )}
       <div className="title color-black mt-3 mb-3">
         Fotos do ambiente interno
       </div>
@@ -68,17 +128,49 @@ export const Anexos = ({
             anexo.get_tipo_documento_display === "Fotos do Ambiente Interno"
         )
         .map((anexo, key) => {
-          return (
+          return !editar ? (
             <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
               <Botao
                 style={BUTTON_STYLE.BLUE_OUTLINE}
                 type={BUTTON_TYPE.BUTTON}
-                className="mr-3"
                 texto={`Anexo ${key + 1}`}
+                className="mr-3"
               />
             </a>
+          ) : (
+            <>
+              <Botao
+                style={BUTTON_STYLE.BLUE_OUTLINE}
+                type={BUTTON_TYPE.BUTTON}
+                icon={BUTTON_ICON.TRASH}
+                className="br-none"
+                onClick={() => removerAnexo(anexo.uuid)}
+              />
+              <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
+                <Botao
+                  style={BUTTON_STYLE.BLUE_OUTLINE}
+                  type={BUTTON_TYPE.BUTTON}
+                  icon={BUTTON_ICON.DOWNLOAD}
+                  className="mr-3"
+                />
+              </a>
+            </>
           );
         })}
+      {editar && (
+        <Field
+          component={InputFile}
+          className="inputfile"
+          texto="Adicionar"
+          name="files"
+          accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
+          setFiles={(files) =>
+            uploadAnexo(files, TIPO_DOCUMENTO.FOTOS_DO_AMBIENTE_INTERNO)
+          }
+          toastSuccess={"Anexo do documento incluído com sucesso!"}
+          multiple
+        />
+      )}
       <div className="title color-black mt-3 mb-3">
         Fotos do ambiente externo
       </div>
@@ -88,17 +180,49 @@ export const Anexos = ({
             anexo.get_tipo_documento_display === "Fotos de Área Externa"
         )
         .map((anexo, key) => {
-          return (
+          return !editar ? (
             <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
               <Botao
                 style={BUTTON_STYLE.BLUE_OUTLINE}
                 type={BUTTON_TYPE.BUTTON}
-                className="mr-3"
                 texto={`Anexo ${key + 1}`}
+                className="mr-3"
               />
             </a>
+          ) : (
+            <>
+              <Botao
+                style={BUTTON_STYLE.BLUE_OUTLINE}
+                type={BUTTON_TYPE.BUTTON}
+                icon={BUTTON_ICON.TRASH}
+                className="br-none"
+                onClick={() => removerAnexo(anexo.uuid)}
+              />
+              <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
+                <Botao
+                  style={BUTTON_STYLE.BLUE_OUTLINE}
+                  type={BUTTON_TYPE.BUTTON}
+                  icon={BUTTON_ICON.DOWNLOAD}
+                  className="mr-3"
+                />
+              </a>
+            </>
           );
         })}
+      {editar && (
+        <Field
+          component={InputFile}
+          className="inputfile"
+          texto="Adicionar"
+          name="files"
+          accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
+          setFiles={(files) =>
+            uploadAnexo(files, TIPO_DOCUMENTO.FOTOS_DE_AREA_EXTERNA)
+          }
+          toastSuccess={"Anexo do documento incluído com sucesso!"}
+          multiple
+        />
+      )}
       <div className="title color-black mt-3 mb-3">
         Certidão de dados cadastrais do imóvel - IPTU
       </div>
@@ -107,7 +231,7 @@ export const Anexos = ({
           (anexo) => anexo.get_tipo_documento_display === "Cópia do IPTU ou ITR"
         )
         .map((anexo, key) => {
-          return (
+          return !editar ? (
             <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
               <Botao
                 style={BUTTON_STYLE.BLUE_OUTLINE}
@@ -116,13 +240,38 @@ export const Anexos = ({
                 className="mr-3"
               />
             </a>
+          ) : (
+            <>
+              <Botao
+                style={BUTTON_STYLE.BLUE_OUTLINE}
+                type={BUTTON_TYPE.BUTTON}
+                icon={BUTTON_ICON.TRASH}
+                className="br-none"
+                onClick={() => removerAnexo(anexo.uuid)}
+              />
+              <a href={anexo.arquivo} target="_blank" rel="noopener noreferrer">
+                <Botao
+                  style={BUTTON_STYLE.BLUE_OUTLINE}
+                  type={BUTTON_TYPE.BUTTON}
+                  icon={BUTTON_ICON.DOWNLOAD}
+                  className="mr-3"
+                />
+              </a>
+            </>
           );
         })}
       {editar && (
-        <Botao
-          icon={BUTTON_ICON.PLUS}
-          style={BUTTON_STYLE.BLUE}
+        <Field
+          component={InputFile}
+          className="inputfile"
           texto="Adicionar"
+          name="files"
+          accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
+          setFiles={(files) =>
+            uploadAnexo(files, TIPO_DOCUMENTO.COPIA_DO_IPTU_ITR)
+          }
+          toastSuccess={"Anexo do documento incluído com sucesso!"}
+          multiple
         />
       )}
       <div className="title color-black mt-3 mb-3">
@@ -164,10 +313,17 @@ export const Anexos = ({
           );
         })}
       {editar && (
-        <Botao
-          icon={BUTTON_ICON.PLUS}
-          style={BUTTON_STYLE.BLUE}
+        <Field
+          component={InputFile}
+          className="inputfile"
           texto="Adicionar"
+          name="files"
+          accept=".png, .doc, .pdf, .docx, .jpeg, .jpg"
+          setFiles={(files) =>
+            uploadAnexo(files, TIPO_DOCUMENTO.COPIA_DE_PLANTA_OU_CROQUI)
+          }
+          toastSuccess={"Anexo do documento incluído com sucesso!"}
+          multiple
         />
       )}
     </>
