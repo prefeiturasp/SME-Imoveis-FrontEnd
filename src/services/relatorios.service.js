7350aa66bb50c7686a234c3904e94546ec79566f
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-03-15 09:19:26 -0300

    Feature: novo service para filtrar relatórios por status

---
+++ src/services/relatorios.service.js
@@ -0,0 +1,47 @@
+import endPonts from "../constants/endPonts.constants";
+import { getToken } from "./auth.service";
+
+
+export const getAnos = () => {
+  const url = `${endPonts.API_URL}/cadastro-imovel/imoveis/anos/`;
+  let status = 0;
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `JWT ${getToken()}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      status = res.status;
+      return res.json();
+    })
+    .then((data) => {
+      return { data: data, status: status };
+    })
+    .catch((error) => {
+      return console.log(error);
+    });
+};
+
+export const filtrar = (params) => {
+  const url = `${endPonts.API_URL}/cadastro-imovel/imoveis/filtrar-por-status/?${params}`;
+  let status = 0;
+  return fetch(url, {
+    method: "GET",
+    headers: {
+      Authorization: `JWT ${getToken()}`,
+      "Content-Type": "application/json",
+    },
+  })
+    .then((res) => {
+      status = res.status;
+      return res.json();
+    })
+    .then((data) => {
+      return { data: data, status: status };
+    })
+    .catch((error) => {
+      return console.log(error);
+    });
+};
