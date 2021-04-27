a7ef8e39ffcc8c946330e4db5a6cb0b5f29ab391
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-04-26 22:38:01 -0300

    Feature: add legenda ao filtrar por status.

--- src/pages/AreaLogada/RelatorioPorStatus/componentes/Filtro/index.jsx
+++ src/pages/AreaLogada/RelatorioPorStatus/componentes/Filtro/index.jsx
@@ -17,6 +17,7 @@ export const Filtro = ({
   setFiltros,
   setResultado,
   setCarregando,
+  setLegenda,
 }) => {
   
   const [opcoesAnos, setOpcoesAnos] = useState([{ label: "Selecione os anos", value: undefined }])
@@ -34,6 +35,85 @@ export const Filtro = ({
       });
   }, []);
 
+  const updateLegenda = () => {
+    if( filtros.status.length !== 0 && filtros.status.length !== 5) {
+      setLegenda(
+        <div className="col-6 legenda">
+          { filtros.status.includes(1) && (
+            <>
+              <p> 
+                Em análise: refere-se a todos os cadastros nos 
+                quais estão em análise onde contempla os seguintes 
+                status de cadastros:
+              </p>
+              <ul>
+                <li>SME Analisou previamente</li>
+                <li>Enviado à COMAPRE</li>
+                <li>Agendamento da vistoria</li>
+                <li>Aguardando relatório de vistoria</li>
+                <li>Aguardando laudo de valor locatício</li>
+              </ul>
+            </>
+          )}
+          { filtros.status.includes(4) && (
+            <>
+              <p> 
+                Finalizados reprovados: refere-se a todos os
+                cadastros nos quais estão finalizados onde 
+                contempla os seguintes status de cadastros:
+              </p>
+              <ul>
+                <li>Finalizado - Área Insuficiente</li>
+                <li>Finalizado - Demanda Insuficiente</li>
+                <li>Finalizado - Não atende as necessidades da SME</li>
+              </ul>
+            </>
+          )}
+          { filtros.status.includes(2) && (
+            <>
+              <p> 
+                Aprovados vistoria: refere-se a todos os cadastros
+                de imóveis aprovados de vistoria onde contempla os
+                seguintes status de cadastros:
+              </p>
+              <ul>
+                <li>Vistoria Aprovada</li>
+                <li>Enviado à DRE</li>
+                <li>Finalizado Aprovado</li>
+              </ul>
+            </>
+          )}
+          { filtros.status.includes(3) && (
+            <>
+              <p> 
+                Reprovados vistoria: refere-se a todos os cadastros de
+                imóveis reprovados de vistoria onde contempla os 
+                seguintes status de cadastros:
+              </p>
+              <ul>
+                <li>Vistoria Reprovada</li>
+                <li>Finalizado Reprovado</li>
+              </ul>
+            </>
+          )}
+          { filtros.status.includes(5) && (
+            <>
+              <p> 
+                Cancelados: refere-se a todos os cadastros nos quais foram 
+                cancelados onde contempla o seguinte status de cadastros:
+              </p>
+              <ul>
+                <li>Cancelado</li>
+              </ul>
+            </>
+          )}
+        </div>
+      );
+    } else {
+      setLegenda( <div className="col-4 legenda"></div>)
+    }
+  }
+
   const onSubmit = async () => {
     setCarregando(true);
     const params = formataPayloadFiltros(filtros);
@@ -42,6 +122,7 @@ export const Filtro = ({
         if (response.status === HTTP_STATUS.OK) {
           setResultado(response.data);
           setCarregando(false);
+          updateLegenda();
         }
       })
       .catch(() => {
