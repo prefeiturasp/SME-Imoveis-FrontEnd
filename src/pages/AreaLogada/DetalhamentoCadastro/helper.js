149fa96433feb042c9f8079a8e8b035c8fb5aff9
rodolphoazeredo <rodolpho.azeredo@amcom.com.br>
2021-09-28 05:13:21 -0300

    Feature: alteração de nome de variáreis utilizadas para o status modificado

--- src/pages/AreaLogada/DetalhamentoCadastro/helper.js
+++ src/pages/AreaLogada/DetalhamentoCadastro/helper.js
@@ -51,22 +51,22 @@ export const formataPaylaodAtualizaCadastro = (values) => {
   return stringParams.slice(0, -1);
 };
 
-export const formataPaylaodEnviarComapre = (values) => {
+export const formataPaylaodEnviarSolicitacaoVistoria = (values) => {
   let stringParams = "&";
   stringParams += `imovel=${values.id}&`;
-  if (values.observacoes_comapre !== undefined && values.observacoes_comapre !== "") {
-    stringParams += `justificativa=${values.observacoes_comapre}&`;
+  if (values.observacoes_solicitacao_vistoria !== undefined && values.observacoes_solicitacao_vistoria !== "") {
+    stringParams += `justificativa=${values.observacoes_solicitacao_vistoria}&`;
   }
-  stringParams += `data_agendada=${values.data_envio_comapre}&`;
+  stringParams += `data_agendada=${values.data_envio_solicitacao_vistoria}&`;
   stringParams += `enviar_email=${values.enviar_email}&`;
   return stringParams.slice(0, -1);
 };
 
-export const formataPaylaodEnviarComapreEdicao = (values) => {
+export const formataPaylaodEnviarSolicitacaoVistoriaEdicao = (values) => {
   let stringParams = "&";
   stringParams += `imovel=${values.id}&`;
-  if (values.observacoes_comapre !== undefined && values.observacoes_comapre !== "") {
-    stringParams += `justificativa=${values.observacoes_comapre}&`;
+  if (values.observacoes_solicitacao_vistoria !== undefined && values.observacoes_solicitacao_vistoria !== "") {
+    stringParams += `justificativa=${values.observacoes_solicitacao_vistoria}&`;
   }
   stringParams += `edicao=ok&`
   return stringParams.slice(0, -1);
@@ -101,8 +101,8 @@ export const formataPayloadFinalizaEdicao = (values) => {
   let stringParams = "&";
   stringParams += `imovel=${values.id}&`;
 
-  if (values.observacoes_comapre !== undefined && values.observacoes_comapre !== "") {
-    stringParams += `justificativa_analise_previa=${values.observacoes_comapre}&`;
+  if (values.observacoes_solicitacao_vistoria !== undefined && values.observacoes_solicitacao_vistoria !== "") {
+    stringParams += `justificativa_analise_previa=${values.observacoes_solicitacao_vistoria}&`;
   }
 
   if (values.observacoes_analise !== undefined && values.observacoes_analise !== "") {
